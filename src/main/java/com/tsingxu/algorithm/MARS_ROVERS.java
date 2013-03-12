7442eeda457c87d6a7323d2b56d5b5644943f75c
tsingxu <3x3h3q@gmail.com>
2013-03-12 20:39:03 +0800

    Signed-off-by: tsingxu <3x3h3q@gmail.com>

--- src/main/java/com/tsingxu/algorithm/MARS_ROVERS.java
+++ src/main/java/com/tsingxu/algorithm/MARS_ROVERS.java
@@ -13,6 +13,8 @@ package com.tsingxu.algorithm;
 public class MARS_ROVERS
 {
 	private static final int[][] step = new int[][] { { 0, 1 }, { 1, 0 }, { 0, -1 }, { -1, 0 } };
+	private static final String[] directionStr = new String[] { "N", "E", "S", "W" };
+	static int X = 0, Y = 0;
 
 	private static class Rover
 	{
@@ -42,6 +44,25 @@ public class MARS_ROVERS
 			{
 				this.direction = (this.direction + 3) % 4;
 			}
+		}
+
+		public void move()
+		{
+			if (this.x + step[this.direction][0] < X)
+			{
+				this.x += step[this.direction][0];
+			}
+
+			if (this.y + step[this.direction][1] < Y)
+			{
+				this.y += step[this.direction][1];
+			}
+
+		}
+
+		public String toString()
+		{
+			return this.x + " " + this.y + " " + directionStr[this.direction];
 		}
 	}
 
